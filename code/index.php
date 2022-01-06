ac43712f3be7d4908dfd82cc567e759dd69ae694
Louis Chu <louis.chu@edu.devinci.fr>
2022-01-06 22:46:51 +0100

    Responsive update

---
+++ code/index.php
@@ -0,0 +1,215 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="head">
+        <img src="img/calque 2.png" alt="">
+        <div class="echangeons">
+            <p><a class="bouton" href="">Echangeons</a></p>
+        </div>
+    </div>
+    <section class="section1">
+        <div class="container1">
+            <div class="container1-1">
+                <h1>Mettez en valeur votre savoir faire en ligne</h1>
+                <p>
+                    Nous nous appuyons sur les outils et médias les plus adaptés à vos besoins pour mettre vos projets en valeur graphiquement.
+                </p>
+            </div>
+            <img class="bureau" src="img/Groupe 36.png" alt="Bureau">
+        </div>
+    </section>
+    <section class="section2">
+        <h1>A chaque besoin sa solution</h1>
+        <div class="container2">
+            <div>
+                <img src="img/Groupe 18.png" alt="">
+                <h2>identité visuelle</h2>
+                <ul>
+                    <li>Logo</li>
+                    <li>Charte graphique</li>
+                    <li>Typographie</li>
+                </ul>
+            </div>
+            <div>
+                <img src="img/Groupe 20.png" alt="">
+                <h2>Web et digital</h2>
+                <ul>
+                    <li>Site internet</li>
+                    <li>UI/UX design</li>
+                    <li>Contenus Social Media</li>
+                    <li>Présentations PPT</li>
+                </ul>
+            </div>
+            <div>
+                <img src="img/Groupe 21.png" alt="">
+                <h2>Communication imprimée</h2>
+                <ul>
+                    <li>Brochure</li>
+                    <li>Magazine</li>
+                    <li>Livre</li>
+                    <li>Campagne d'affiche</li>
+                </ul>
+            </div>
+            <div>
+                <img src="img/Groupe 24.png" alt="">
+                <h2>Prototype</h2>
+                <ul>
+                    <li>WebApp</li>
+                    <li>Application</li>
+                    <li>Réalité Augmentée</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="button_s2">
+        <a href="">Echangeons !</a>
+    </div>
+    <section class="section3"> 
+        <div class="title_s3">
+            <h1>Nous vous accompagnons à <br> chaque étape de votre projet</h1>
+        </div>
+        <div class="align_center">
+            <img src="img/dino.png" alt="dino" class="dino">
+        </div>
+        <div class="container_s3">
+            <div class="text_s3">
+                <h2>Analyse</h2>
+                <p>Chaque solution à vos besoins commence par une analyse poussée de votre situation pour des projets 100% cohérents avec vos objectifs.</p>
+            </div>
+            <div class="text_s3">
+                <h2>Accompagnement</h2>
+                <p>Parce que chaque projet est unique, nous nous adaptons à chaque besoins et chaque situations, en restant toujours transparents et en vous accompagnant tout au long de notre parcours avec vous.</p>
+            </div>
+            <div class="text_s3">
+                <h2>Experience</h2>
+                <p>Notre objectif : faire de vos projets des expériences justes, cohérentes et accessibles, en mettant au centre le lien humain, essentiel à une communication transparente.</p>
+            </div>
+            <div class="text_s3">
+                <h2>Créativité</h2>
+                <p>Nous mettons nos compétences graphiques et de gestion au profit de projets créatifs qui vous ressemblent.</p>
+            </div>
+        </div>
+        <div class="echangeons_s3">
+            <a class="bouton" href="">Echangeons</a>
+        </div>
+    </section>
+    <section class="section4">
+        <div class="container_s4">
+            <div class="bloc1_s4">
+                <img src="img/Sans-titre-2.png" alt="Koss Sport" class="koss">
+                <div class="text1_s4">
+                    <h2>Koss Sport</h2>
+                    <p>Identité visuelle & site web</p>
+                </div>
+            </div>
+            <p class="text2_s4">" Notre collaboration avec le Studio Edoras
+                nous a permis d'avoir un résultat au delà de nos attentes. Nous avons pu bénéficier d'un accompagnements sur tous les plans avec des échanges et une progression rapide. "</p>
+            <p class="text2_s4" style="text-indent: 85%;">Thibault</p>
+        </div>
+        <div class="image_container">
+            <img src="img/e-1.png" alt="Koss Sport" class="image_s4">
+        </div>
+    </section>
+
+    <!-- Creation section "6" Identité  -->
+    <section class="se6">
+        <div class="se6flex">
+            <div class="se6gauche">
+                <h3 class="se6h3">Une identité forte et fédératrice, proche de vos valeurs</h3>
+                <p class="se6p">Basé sur une stratégie d'identité visuelle en lien avec vos valeurs et vos clients, nous adaptons votre image en la rendant plus forte et fédératrice, durable dans le temps</p>
+                <button class="se6b">Echangeons !</button>
+            </div>
+            <div class="se6droite">
+                <img class="se6img" src="img/se6id.png" alt="Identité">
+            </div>
+        </div>
+        <div class="se6bas">
+            <p>10 secondes suffisent à une personne pour avoir une opinion sur un logo, un branding cohérent augmente considérablement vos ventes.</p>
+        </div>
+        
+
+    </section>
+    
+
+    <!-- Creation section "7" Experiences digitales  -->
+    <section class="se7">
+        <nav class="se7nav">
+            <img src="img/se71.png" alt="Tablettes">
+            <img src="img/se72.png" alt="Affiches">
+            <img src="img/se73.png" alt="Journaux">
+            <img src="img/se74.png" alt="Computeurs">
+            <img src="img/se75.png" alt="Mobiles">
+        </nav>
+        <div class="se7flex">
+            <div class="se7gauche">
+                <h3 class="se7h3">Des expériences digitales adaptées à vos utilisateurs</h3>
+                <p class="se7p">Sites, applications, contenus social media… Nous vous proposons des supports de communication digitaux offrant une expérience utilisateurs intuitive, agréable et surtout engageante.</p>
+            </div>
+            <div class="se7droite">
+                <img class="se7img" src="img/se7pantin.png" alt="Digitales">
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Creation section "8" Support de communication  -->
+    <section class="se8">
+        <div class="se8gauche">
+            <img class="se8img" src="img/se8pieuvre.png" alt="Communication">
+        </div>
+
+        <div class="se8droite">       
+            <h3 class="se8h3">Des supports de communications cohérents qui parlent à vos cibles</h3>
+            <p class="se8p">Qu'ils soient imprimés ou digitaux, nous vous accompagnons dans la création de vos supports afin de rendre votre communication homogène, cohérente et fidèle à votre image</p>
+        </div>
+
+    </section>
+
+
+    <!-- Creation du pied de page -->
+    <footer>
+        <h2>Echangeons !</h2>
+        <p class="footp">Choisissez sur quelle plateforme voulez-vous faire le rendez-vous ?</p>
+        <!-- Création d'une nav pour choisir comment contacter l'agence -->
+        <nav class="footnav">
+            <!-- Mail -->
+            <button class="footb" onclick="window.location.href='mail.html'">
+                <div class="footbghover">
+                    <img src="img/footenveloppe.png" alt="Mail">
+                </div>                 
+            </button>
+            <!-- Discord -->
+            <button class="footb" onclick="window.location.href='discord.html'">
+                <div class="footbghover"> 
+                    <img src="img/footdiscord.png" alt="Discord">
+                </div>
+            </button>
+            <!-- Teams -->
+            <button class="footb" onclick="window.location.href='teams.html'">
+                <div class="footbghover">
+                    <img src="img/footteams.png" alt="Microsoft-Teams">
+                </div>               
+            </button>
+            <!-- Téléphone/message -->
+            <button class="footb" onclick="window.location.href='phone.html'">
+                <div class="footbghover">
+                    <img src="img/footappel.png" alt="Téléphone">
+                </div>                                
+            </button>
+        </nav>
+        <p class="footp">Ne communiquez plus, <strong> faites rêver !</strong></p>
+        <p class="footdroits">Studio Edoras © 2020-2024 - Tous droits réservés</p>
+        <img class="footimgcont" src="img/foottel.png" alt="Contact">
+
+
+
+    </footer>
+</body>
+</html>
